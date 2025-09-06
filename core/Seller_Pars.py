2cfd2eab58d3f06f8fabf2b70b03b7792e9f3a75
DarkDescent <koner.ner@mail.ru>
2025-09-07 02:23:32 +0300

    Sellers get id

---
+++ core/Seller_Pars.py
@@ -0,0 +1,15 @@
+import json
+
+
+class Sellers:
+
+     def get_idSeller(self,name_seller):
+         with open("E:\\Projects\\Wb_parse_NEW\\brands.json","r",encoding="UTF-8") as brands_js:
+            brands = json.load(brands_js)
+            for items in brands:
+                if name_seller.lower() == items["Поставщик"].lower():
+                    return items["SupplierID"]
+
+
+
+seller = Sellers().get_idSeller("мЬюзик хамер")
